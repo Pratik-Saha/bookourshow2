3cdcb355db8f24d4e716cc2a2750457e0e45ab14
Pratik-Saha <35095559+Pratik-Saha@users.noreply.github.com>
2019-12-27 18:56:59 +0530

    Create ShowUserPage.js

---
+++ BloggerUI/ShowUserPage.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+class ShowUserPage extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            userName: '',
+            posts: []
+        }
+    }
+
+    componentDidMount(){
+        const id = this.props.match.params.id
+        let userName = ''
+        let posts = []
+        axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then((response) => {
+                userName = response.data.name
+                axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+                .then((response) => {
+                    response.data.map((post) => {
+                        posts.push(post)
+                       this.setState({userName,posts})
+                    })
+                })
+            })
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>USER NAME : {this.state.userName}</h2>
+                <h2>POSTS WRITTEN BY USER</h2>
+                <ul>
+                    {
+                        this.state.posts.map((post) => {
+                            return <li key = {post.id}><Link to = {`/posts/${post.id}`}>{post.title}</Link></li>
+                        })
+                    }
+                </ul>
+                
+            </div>
+        )
+    }
+}
+export default ShowUserPage
