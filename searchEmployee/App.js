407e9f9420ceee80ee94b30759aed32569f4ee54
Pratik-Saha <35095559+Pratik-Saha@users.noreply.github.com>
2020-02-11 20:06:59 +0530

    Create App.js

---
+++ searchEmployee/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ShowEmployee from './Employee'
+
+function App() {
+  return (
+    <div>
+      <h2>Search Employee</h2>
+      <ShowEmployee />
+    </div>
+  );
+}
+
+export default App;
